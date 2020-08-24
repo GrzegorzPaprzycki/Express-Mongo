79963f92fff8fddb4413989663a1b6c652a25ff1
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-08-24 19:45:56 +0200

    Lesson 95 & 96 - admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('/login');
+        return;
+    }
+    next();
+})
+
 /* GET admin */
 router.get('/', (req, res) => {
+    console.log(req.session.admin);
     res.render('admin', { title: 'Admin' });
 });
 
