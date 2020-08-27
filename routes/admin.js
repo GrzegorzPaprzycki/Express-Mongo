dba3aef54229f1d1c1468533149e331534376b7b
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-08-27 20:03:12 +0200

    Lesson 100 - complete news form

--- routes/admin.js
+++ routes/admin.js
@@ -12,20 +12,34 @@ router.all('*', (req, res, next) => {
 
 /* GET admin */
 router.get('/', (req, res) => {
-    // const exampleNews = new News({
-    //     title: 'Przykladowy tytul',
-    //     description: 'Przykladowy opisasdf'
-    // })
-
-    // exampleNews.save((err) => {
-    //     console.log(err)
-    // })
-
-    res.render('admin/index', { title: 'Admin' });
+    News.find({}, (err, news) => {
+        res.render('admin/index', { title: 'Admin', news });
+    })
 });
 
 router.get('/news/add', (req, res) => {
-    res.render('admin/news-form', { title: 'Dodaj news' });
+    res.render('admin/news-form', { title: 'Dodaj news', body: {} });
+})
+
+router.post('/news/add', (req, res) => {
+    const body = req.body;
+    const newsData = new News(body)
+
+    const bugs = newsData.validateSync();
+
+    newsData.save((err) => {
+        if (err) {
+            res.render('admin/news-form', { title: 'Dodaj news', bugs, body });
+        } else {
+            res.redirect('/admin');
+        }
+    })
+})
+
+router.get('/news/delete/:id', (req, res) => {
+    News.findByIdAndDelete(req.params.id, (err) => {
+        res.redirect('/admin');
+    });
 })
 
 module.exports = router;
