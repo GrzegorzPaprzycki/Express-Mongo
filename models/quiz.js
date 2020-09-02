cc5d15bbcfcace867df8133325f4fcc28e4e3967
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-09-02 13:14:17 +0200

    Delete all files
    Whole working area has been cleaned.
    Project starts from the beggining.

--- models/quiz.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const quizSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    vote: { type: Number, required: true, default: 0 },
-});
-
-module.exports = mongoose.model('Quiz', quizSchema);
