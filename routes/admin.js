518ce9c9c8517e8cab2473e02dad233fdc6c65b0
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-09-14 17:35:11 +0200

    Lesson 99 - news form page
    News form page created.

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,14 @@ const router = express.Router();
 /* GET admin page. */
 router.get('/', (req, res) => {
     if (req.session.admin) {
-        res.render('admin', { title: 'Admin' });
+        res.render('admin/index', { title: 'Admin' });
     } else {
         res.redirect('/login');
     }
 });
+
+router.get('/news/add', (req, res) => {
+    res.render('admin/news-form', { title: 'Admin - dodawanie atrykułu' })
+})
 
 module.exports = router;
